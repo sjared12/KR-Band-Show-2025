c640b6342ec599225c97d6d2fc4a6ce95c6c3725
sjared12 <jared.smith12@outlook.com>
2025-09-09 19:28:13 -0400

    Updated nav bar to hide

--- src/hide-concessions-link.js
+++ src/hide-concessions-link.js
@@ -3,7 +3,7 @@
   var showTime = new Date('2025-10-25T20:00:00Z');
   var now = new Date();
   if (now < showTime) {
-    var links = document.querySelectorAll('a[href="concessions.html"]');
+    var links = document.querySelectorAll('a[href="concessions.html"], a[href="/concessions"]');
     links.forEach(function(link) {
       link.style.display = 'none';
     });
